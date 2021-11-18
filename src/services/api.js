d80f8b7c9fe389e8d383e24a855fca491a3b8073
Jose <josetoitio@gmail.com>
2021-11-18 17:21:26 -0300

    resolve package-lock.json

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,6 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
