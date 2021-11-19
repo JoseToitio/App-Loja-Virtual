056bf4b67553e7d6bca652e1e5937657a52487c7
raffael marinho <silvamarinho10@gmail.com>
2021-11-18 22:38:30 -0300

    feat:add requeriment 4

---
+++ src/components/Categorias.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Categorias(prop) {
+  const { name, id } = prop;
+  return (
+    <div>
+      <label htmlFor={ id }>
+        {name}
+        <input type="radio" data-testid="category" id={ id } value={ name } />
+      </label>
+    </div>
+  );
+}
--- src/components/PaginaPrincipal.jsx
+++ src/components/PaginaPrincipal.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Categorias from './Categorias';
 
 class PaginaPrincipal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Categoris: [] };
+  }
+
+  componentDidMount() {
+    const promissesApi = fetch(
+      'https://api.mercadolibre.com/sites/MLB/categories',
+    );
+    promissesApi
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({ Categoris: res });
+      });
+    promissesApi.catch(() => console.log('deu ruim'));
+  }
+
   render() {
+    const { Categoris } = this.state;
     return (
       <div>
         <input type="text" />
-        <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho</Link>
+        <Link to="/carrinho" data-testid="shopping-cart-button">
+          Carrinho
+        </Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <div>
+          {Categoris.map((categoria) => (
+            <Categorias
+              name={ categoria.name }
+              key={ categoria.id }
+              id={ categoria.id }
+            />
+          ))}
+        </div>
       </div>
     );
   }
