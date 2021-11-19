8da8ae13cbf5a221bcd74177fa20619a8e71d05f
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-11-19 16:39:26 -0300

    add parte requsito 5

--- src/components/PaginaPrincipal.jsx
+++ src/components/PaginaPrincipal.jsx
@@ -1,28 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Categorias from './Categorias';
-import { getCategories } from '../services/api';
+import Card from './Card';
+import {
+  getCategories,
+  getProductsFromCategoryAndQuery,
+} from '../services/api';
 
 class PaginaPrincipal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { categories: [] };
+    this.state = {
+      categories: [],
+      inputTextValue: '',
+      inputCheckValue: '',
+      products: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.getProducts = this.getProducts.bind(this);
   }
 
   componentDidMount() {
-    getCategories()
-      .then((data) => {
-        this.setState({
-          categories: data,
-        });
+    getCategories().then((data) => {
+      this.setState({
+        categories: data,
       });
+    });
+    this.getProducts();
+  }
+
+  handleChange({ target: { value, type, id } }) {
+    if (type === 'radio') {
+      this.setState({ inputCheckValue: id });
+    } else {
+      this.setState({ inputTextValue: value });
+    }
+  }
+
+  getProducts() {
+    const { inputTextValue, inputCheckValue } = this.state;
+    getProductsFromCategoryAndQuery(inputCheckValue, inputTextValue).then(
+      (data) => {
+        this.setState({ products: data.results });
+      },
+    );
   }
 
   render() {
     const { categories } = this.state;
     return (
       <div>
-        <input type="text" />
+        <input type="text" onChange={ this.handleChange } />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.getProducts }
+        >
+          Pesquisar
+        </button>
         <Link to="/carrinho" data-testid="shopping-cart-button">
           Carrinho
         </Link>
@@ -35,6 +70,7 @@ class PaginaPrincipal extends React.Component {
               name={ categoria.name }
               key={ categoria.id }
               id={ categoria.id }
+              handleChange={ this.handleChange }
             />
           ))}
         </div>
